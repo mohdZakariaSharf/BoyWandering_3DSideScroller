82bf23959dc781f169c799472bc6252a7fdf17fd
BuildTools <unconfigured@null.spigotmc.org>
2022-01-15 21:36:18 +0800

    3DSideScroller Final Version

---
+++ Assets/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject c;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - c.transform.position;
+    }
+
+    // Last Function called every frame
+    void LateUpdate()
+    {
+        transform.position = c.transform.position + offset;
+    }
+}
---
+++ Assets/menuScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menuScript : MonoBehaviour
+{
+    public GameObject panel;
+
+    void Update()
+    {
+
+    }
+    public void StartButton()
+    {
+        SceneManager.LoadScene("sideScroll");
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
